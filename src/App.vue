5b5aa2e133c443cd8de5418936c876e944e6427c
Ben George <ben@onlinelasercutting.com.au>
2020-09-03 22:43:45 +1000

    Devtools debug demo

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template>
+  <div id="app">
+    <!-- This is the root of your panel -->
+    <!-- Content should go inside #app -->
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue CLI panel" />
+  </div>
+</template>
+
+<script>
+// You can access this App.vue file from any other component via `this.$root.$children[0]`
+// See `./components/HelloWorld.vue` for example of CSInterface and this.app
+
+// Create your own components and import them here
+import HelloWorld from "./components/HelloWorld.vue";
+
+// Dynamic CSS variables that automatically handle all app themes and changes:
+// https://github.com/Inventsable/starlette
+// import starlette from "starlette";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    csInterface: null
+  }),
+  mounted() {
+    // this.csInterface = new CSInterface();
+    // starlette.init();
+  },
+  methods: {
+    // dispatchEvent(name, data) {
+    //   var event = new CSEvent(name, "APPLICATION");
+    //   event.data = data;
+    //   this.csInterface.dispatchEvent(event);
+    // },
+    // loadScript(path) {
+    //   this.csInterface.evalScript(`$.evalFile('${path}')`);
+    // }
+  }
+};
+</script>
+
+<style>
+/* Various helper styles to match application theme */
+@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
+:root {
+  background-color: var(--color-bg);
+  color: var(--color-default);
+  font-family: "Open Sans", sans-serif;
+  font-size: 10px;
+}
+
+#app::-webkit-scrollbar {
+  display: block;
+}
+body::-webkit-scrollbar {
+  width: 0px;
+}
+
+::-webkit-scrollbar {
+  background-color: var(--color-scrollbar);
+  width: var(--width-scrollbar-track);
+}
+::-webkit-scrollbar-thumb {
+  width: var(--width-scrollbar-track);
+  background: var(--color-scrollbar-thumb);
+  border-radius: var(--radius-scrollbar-thumb);
+}
+::-webkit-scrollbar-thumb:hover {
+  background: var(--color-scrollbar-thumb-hover);
+}
+::-webkit-scrollbar-resizer {
+  display: none;
+  width: 0px;
+  background-color: transparent;
+}
+::-webkit-scrollbar-button {
+  height: 0px;
+}
+::-webkit-scrollbar-corner {
+  display: none;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1 style="color:white">{{ test }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data(){
+    return {
+      test: "bobo"
+    }
+  },
+  computed: {
+    // Use as this.app
+    app() {
+      return this.$root.$children[0];
+    }
+  },
+  mounted() {
+    console.log("Top-level root instance (App.vue):");
+    console.log(this.app);
+    // console.log("CSInteface:");
+    // console.log(this.cs);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
